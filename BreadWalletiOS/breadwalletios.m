d665ec6cdc91071fb6979914e07eb15410682ce2
Alon Muroch <alonmuroch@gmail.com>
2015-07-11 14:27:32 +0300

    small fixes

---
+++ BreadWalletiOS/breadwalletios.m
@@ -0,0 +1,13 @@
+//
+//  breadwalletios.m
+//  breadwalletios
+//
+//  Created by alon muroch on 7/8/15.
+//  Copyright (c) 2015 Aaron Voisine. All rights reserved.
+//
+
+#import "breadwalletios.h"
+
+@implementation breadwalletios
+
+@end
---
+++ breadwalletiOSTests/BreadWalletiOSTests.m
@@ -0,0 +1,40 @@
+//
+//  BreadWalletiOSTests.m
+//  BreadWalletiOSTests
+//
+//  Created by alon muroch on 7/8/15.
+//  Copyright (c) 2015 Aaron Voisine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface BreadWalletiOSTests : XCTestCase
+
+@end
+
+@implementation BreadWalletiOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
